ece414274a9270ab8f3c709b5d8b74360384e097
mattjeon <coolmattjeon@gmail.com>
2023-12-14 12:27:25 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,14 @@
+가이드 - [바이오커넥트]상명대학교android가이드.docx
+
+카메라, 얼굴 detector, skin segmentation, 서버 통신 코드 & AAR 파일 생성 안드로이드 스튜디오 프로젝트 
+
+
+* https가 아닌 서버접속을 위해 프로젝트별 res/xml/network_security_config.xml 파일의 
+<network-security-config>
+    <base-config cleartextTrafficPermitted="true" />
+</network-security-config>
+로 설정 
+
+* local.properties 
+base_url="http://118.128.153.171:8088"  //사내 파일서버 경로
+
---
+++ app/src/main/java/com/bioconnect/vitaltracker/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.bioconnect.vitaltracker
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import com.bioconnect.vitaltracker.ui.theme.BioconnectTheme
+
+import com.example.bioconnect.MeasureView
+import com.example.bioconnect.data.HealthData
+
+// option
+import com.example.bioconnect.utils.ESTIMATE_TIME
+import com.example.bioconnect.utils.GET_LOG
+import com.example.bioconnect.utils.stressToLevel
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BioconnectTheme {
+                // 측정 결과 변수
+                val result = remember{
+                    MutableStateFlow(
+                        Pair<Boolean, HealthData?>(false, null)
+                    )
+                }
+
+                // setting estimate time : Int (default : 15)
+                ESTIMATE_TIME = 15
+
+                // setting communication log flag : Boolean
+                GET_LOG = true
+
+                // 측정 결과 리스너
+                LaunchedEffect(key1 = result.collectAsState().value){
+
+                    val resultOk = result.value.first
+                    Log.e("Test>>", "resultOk: ${resultOk}")
+
+                    if (resultOk){
+                        Log.e("Test>>", "result: ${result.value.second}")
+
+                        // stress index to stress level
+                        Log.e("Test>>", stressToLevel(result.value.second!!.stress))
+                    }
+                }
+
+                MeasureView(
+                    activity = this@MainActivity,
+                    baseUrl = "http://118.128.153.171:8088",
+                    showResultTable = true,
+//                    resultOk = resultOk,
+                    result = result
+                )
+            }
+        }
+    }
+}
